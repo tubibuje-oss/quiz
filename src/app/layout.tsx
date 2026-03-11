275f628ad4cc1488c1421f6e2c0337ee237a97fa
25LP2689 25LP2689 <25LP2689@G302-CC0108.local>
2026-03-11 20:11:55 +0800

    b

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 const clerkPubKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -30,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider publishableKey={clerkPubKey}>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className="antialiased">
           <SidebarProvider>{children}</SidebarProvider>
         </body>
       </ClerkProvider>
